f35110b41dca168e67244339273aca5cdf413ed5
jaimezzapata <zapataval2304@gmail.com>
2025-05-21 23:15:29 -0500

    feat: definición de enrutador de la aplicaci´n

---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,65 @@
+import ConsumoAPI from "../pages/ConsumoAPI";
+import Contexto from "../pages/Contexto";
+import HooksBasicos from "../pages/HooksBasicos";
+import Introduccion from "../pages/Introduccion";
+import JSXProps from "../pages/JSXProps";
+import Login from "../pages/Login";
+import ProyectoIntegrador from "../pages/ProyectoIntegrador";
+import PruebasUnitarias from "../pages/PruebasUnitarias";
+import React from "../pages/React";
+import Rutas from "../pages/Rutas";
+import SPA from "../pages/SPA";
+import ProtectedRoute from "./ProtectedRoute";
+
+export let enrutadorApp = [
+    {
+        path: "/",
+        element: <Login />
+    },
+    {
+        path: "/home/",
+        element: <ProtectedRoute proteger={<Home />} />,
+        children: [
+            {
+                path: "/consumo-api",
+                element: <ConsumoAPI />
+            },
+            {
+                path: "/contexto",
+                element: <Contexto />
+            },
+            {
+                path: "/hooks-basicos",
+                element: <HooksBasicos />
+            },
+            {
+                path: "/introduccion",
+                element: <Introduccion />
+            },
+            {
+                path: "/jsx-props",
+                element: <JSXProps />
+            },
+            {
+                path: "/proyecto-integrador",
+                element: <ProyectoIntegrador />
+            },
+            {
+                path: "/pruebas-unitarias",
+                element: <PruebasUnitarias />
+            },
+            {
+                path: "/react",
+                element: <React />
+            },
+            {
+                path: "/rutas",
+                element: <Rutas />
+            },
+            {
+                path: "/spa",
+                element: <SPA />
+            }
+        ]
+    }
+]
---
+++ src/routes/ProtectedRoute.jsx
@@ -0,0 +1,8 @@
+
+const ProtectedRoute = ({proteger}) => {
+  return (
+    <div>ProtectedRoute</div>
+  )
+}
+
+export default ProtectedRoute
